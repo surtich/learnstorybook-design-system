1ab71c34593e53055ade9b94f795ec094fd9e590
Javier Pérez Arteaga <surtich@gmail.com>
2019-10-11 02:28:48 +0200

    Added docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -10,4 +10,4 @@ addDecorator(story => (
 ));
 
 // automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+configure(require.context("../src", true, /\.stories\.(js)$/), module);
